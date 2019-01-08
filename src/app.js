ea9047632929d67adac34702475d09081e0b1a78
hltdev8642 <39349712+hltdev8642@users.noreply.github.com>
2019-01-08 12:39:45 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,46 @@
-const {Button, TextView, ui} = require('tabris');
+const {Picker, TextView, ui} = require('tabris');
 
-let button = new Button({
-  centerX: 0, top: 100,
-  text: 'Show message'
+const THEMES = ['default', 'light', 'dark'];
+const DISPLAY_MODES = ['default', 'float', 'hide'];
+const BACKGROUNDS = [ui.statusBar.background, 'rgba(0, 0, 0, 0.25)', 'red', 'green', 'blue'];
+
+createTextView('Theme', 'theme');
+createTextView('Display mode', 'displayMode');
+createTextView('Background', 'background');
+createTextView('Height', 'height');
+
+new Picker({
+  left: '#displayMode 16', baseline: '#theme', right: 16,
+  itemCount: THEMES.length,
+  itemText: index => THEMES[index]
+}).on('select', ({index}) => ui.statusBar.theme = THEMES[index])
+  .appendTo(ui.contentView);
+
+new Picker({
+  left: '#displayMode 16', baseline: '#displayMode', right: 16,
+  itemCount: DISPLAY_MODES.length,
+  itemText: index => DISPLAY_MODES[index]
+}).on('select', ({index}) => ui.statusBar.displayMode = DISPLAY_MODES[index])
+  .appendTo(ui.contentView);
+
+new Picker({
+  left: '#displayMode 16', baseline: '#background', right: 16,
+  itemCount: BACKGROUNDS.length,
+  itemText: index => BACKGROUNDS[index]
+}).on('select', ({index}) => ui.statusBar.background = BACKGROUNDS[index])
+  .appendTo(ui.contentView);
+
+new TextView({
+  left: '#displayMode 16', baseline: '#height', right: 16,
+  text: ui.statusBar.height
 }).appendTo(ui.contentView);
 
-let textView = new TextView({
-  centerX: 0, top: [button, 50],
-  font: '24px'
-}).appendTo(ui.contentView);
+function createTextView(text, id) {
+  new TextView({
+    id: id,
+    left: 16, top: 'prev() 16',
+    text: text
+  }).appendTo(ui.contentView);
+}
 
-button.on('select', () => {
-  textView.text = 'Tabris.js rocks!';
-});
+ui.statusBar.on('tap', () => console.log('Status bar tapped'));
